9b7166daed26abbfcced13750a11e3b770b02d9b
Alban Bruder <alban.bruder@uni-weimar.de>
2020-11-25 23:11:33 +0100

    Add documentation

--- framework/include/camera_node.hpp
+++ framework/include/camera_node.hpp
@@ -8,18 +8,40 @@
 
 class CameraNode : public Node {
   public:
+    // constructors
     CameraNode();
     CameraNode(std::string name);
+
     bool getPerspective() const;
+
+    /**
+     * Is this camera enabled?
+     */
     bool getEnabled() const;
+
+    /**
+     * Enables this camera
+     */
     void setEnabled(bool enabled);
+
+    /**
+     * Get the projection matrix of this camera
+     */
     glm::mat4 getProjectionMatrix() const;
+
+    /**
+     * Set the projection matrix of this camera
+     */
     void setProjectionMatrix(const glm::mat4 &newProjectionMatrix);
 
   
   private:
     bool isPerspective;
+
+    // enabled or not
     bool isEnabled;
+
+    // projection matrix of the camera
     glm::fmat4 projectionMatrix;
 
 };
--- framework/include/scene_graph.hpp
+++ framework/include/scene_graph.hpp
@@ -7,16 +7,48 @@
 
 class SceneGraph {
   public:
+    // constructors
     SceneGraph();
+
+    /**
+     * Get the name of this scene
+     */
     std::string getName() const;
+
+    /**
+     * Get the root node of this scene
+     */
     std::shared_ptr<Node> getRoot() const;
+
+    /**
+     * Print the scene graph
+     */
     std::string printGraph() const;
+
+    /**
+     * TODO: replace with multi camera system
+     */
     std::shared_ptr<CameraNode> camera = nullptr;
 
   private:
+    /**
+     * Set the name of this scene
+     */
     void setName(std::string newName);
+
+    /**
+     * Set the root node of this scene
+     */
     void setRoot(const Node &newRoot);
+
+    /**
+     * The name of this scene
+     */
     std::string name;
+
+    /**
+     * The root node of this scene
+     */
     std::shared_ptr<Node> root;
 };
 
